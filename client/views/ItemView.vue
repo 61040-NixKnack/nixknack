e7d9d8c6e20b9180d637f536a626123bb4001f42
Caleb Frieson <cnf@mit.edu>
2023-11-28 23:35:55 -0500

    Added Catalog Component Folder and Item Page

---
+++ client/views/ItemView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const itemID = ref("");
+
+onBeforeMount(() => {
+  const route = useRoute();
+  itemID.value = route.params.id as string;
+});
+</script>
+
+<template>
+  <main class="column">
+    <p>Put like... the item thing you make here. with the like.. thing attached so it knows what item it is</p>
+    <p>{{ itemID }}</p>
+  </main>
+</template>
