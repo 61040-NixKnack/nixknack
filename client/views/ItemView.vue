70289d0d16bcff5740e9633c21cbe877222d2eb0
Caleb Frieson <cnf@mit.edu>
2023-11-29 00:06:58 -0500

    Created ItemInfo Component

--- client/views/ItemView.vue
+++ client/views/ItemView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from "vue";
 import { useRoute } from "vue-router";
+import ItemInfo from "@/components/Catalog/ItemInfo.vue";
 
 const itemID = ref("");
 
@@ -12,7 +13,6 @@ onBeforeMount(() => {
 
 <template>
   <main class="column">
-    <p>Put like... the item thing you make here. with the like.. thing attached so it knows what item it is</p>
-    <p>{{ itemID }}</p>
+    <ItemInfo :itemID="itemID" />
   </main>
 </template>
