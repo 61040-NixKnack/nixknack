ae99ad742f1617282326945c5f1308d4507e6b85
Adrian Anaemeje <anaemeje@mit.edu>
2023-11-30 12:55:45 -0500

    Create CatalogView and add FAB

---
+++ client/views/CatalogView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+
+const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
+</script>
+
+<template>
+  <main>
+    <h1>Your KnickKnacks</h1>
+    <button id="new-post-fab" class="material-symbols-outlined" @click="console.log('Add item button clicked')">add</button>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 8px 36px;
+}
+
+h1 {
+  text-align: left;
+  color: var(--on-primary);
+}
+
+#new-post-fab {
+  position: fixed;
+  bottom: 80px;
+  right: 0;
+
+  border: none;
+  background-color: var(--dark-accent);
+  color: var(--on-primary);
+
+  font-size: 24px;
+  border-radius: 16px;
+  width: 56px;
+  height: 56px;
+  margin: 0 16px 16px 0;
+  cursor: pointer;
+}
+</style>
