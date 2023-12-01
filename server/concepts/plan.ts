c59b3d4e4347ec4089f5aeb8f53bfa5d40ad91a2
Lauren Wong <laurensw@mit.edu>
2023-12-01 16:23:46 -0500

    implement getTasksAtDate and expireTask for plan concept

--- server/concepts/plan.ts
+++ server/concepts/plan.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
+import { NotFoundError } from "./errors";
 
 const TASKS_PER_DAY = 3;
 
@@ -20,9 +21,37 @@ export default class PlanConcept {
     return res;
   }
 
+  private normalizeDate(date: Date) {
+    const dateCopy = new Date(date.getTime());
+    dateCopy.setHours(0);
+    dateCopy.setMinutes(0);
+    dateCopy.setSeconds(0);
+    return dateCopy;
+  }
+
+  async getTasksAtDate(user: ObjectId, date: Date) {
+    const deadline = this.normalizeDate(date);
+    const plan = await this.plans.readOne({ user, deadline });
+
+    if (!plan) {
+      throw new NotFoundError(`Plan for user ${user} and date ${date} not found!`);
+    }
+
+    return plan.tasks;
+  }
+
   async create(user: ObjectId, deadline: Date, tasks: ObjectId[]) {
     const selected: ObjectId[] = this.choose(tasks, TASKS_PER_DAY);
     const _id = await this.plans.createOne({ user, deadline, tasks: selected });
     return { msg: "User created successfully!", plan: await this.plans.readOne({ _id }) };
+  }
+
+  async expireTask(user: ObjectId) {
+    const curDate = this.normalizeDate(new Date());
+
+    await this.plans.deleteMany({
+      user,
+      deadline: { $lt: curDate },
+    });
   }
 }
