24ba3562376b73f1ce6e05feb324b79925929e3b
Adrian Anaemeje <anaemeje@mit.edu>
2023-12-01 21:40:56 -0500

    Add functionality to AddItemForm component

--- client/components/AddItem/AddItemForm.vue
+++ client/components/AddItem/AddItemForm.vue
@@ -1,33 +1,33 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { fetchy } from "../../utils/fetchy";
 import "@material/web/button/filled-button.js";
 import "@material/web/textfield/outlined-text-field.js";
+import { ref } from "vue";
+import { fetchy } from "../../utils/fetchy";
+import { formatDateShort } from "../../utils/formatDate";
 const name = ref("");
-const lastUsedDate = ref("");
+const lastUsedDate = ref(formatDateShort(new Date()));
 const location = ref("");
 const purpose = ref("");
 
 const emit = defineEmits(["closeSheet"]);
 
-const makeFake = async () => {
-  await fetchy("/api/items", "POST", {
-    body: { name: "Test Item", purpose: "This is a test description. Real descriptions are more helpful. Just padding this out to be a bit longer", lastUsedDate: new Date().toString() },
-  });
-};
+// const makeFake = async () => {
+//   await fetchy("/api/items", "POST", {
+//     body: { name: "Test Item", purpose: "This is a test description. Real descriptions are more helpful. Just padding this out to be a bit longer", lastUsedDate: new Date().toString() },
+//   });
+// };
 
 const createItem = async (name: string, lastUsedDate: string, location: string, purpose: string) => {
   try {
-    // await fetchy("/api/items", "POST", {
-    //   body: { content, name, lastUsedDate, location, purpose },
-    // });
+    await fetchy("/api/items", "POST", {
+      body: { name, lastUsedDate, location, purpose },
+    });
 
-    await makeFake();
     emit("closeSheet");
   } catch (_) {
     return;
   }
-  //   void updateStatus();
+
   emptyForm();
 };
 
@@ -43,10 +43,10 @@ const emptyForm = () => {
   <div class="creation-form">
     <form @submit.prevent="createItem(name, lastUsedDate, location, purpose)">
       <div class="creation-form-header">
-        <h2 class="hint-text">Add a KnickKnack (Submit form for a test item!)</h2>
+        <h2 class="hint-text">Add a KnickKnack</h2>
       </div>
       <img src="@/assets/images/noImage.png" alt="No image" class="default-image" />
-      <md-outlined-text-field v-model="name" label="Name" placeholder="Name"></md-outlined-text-field>
+      <md-outlined-text-field required v-model="name" label="Name" placeholder="Name"></md-outlined-text-field>
       <md-outlined-text-field pattern="\d{2}/\d{2}/\d{4}" v-model="lastUsedDate" label="Last Used Date" placeholder="mm/dd/yyyy"></md-outlined-text-field>
       <md-outlined-text-field v-model="location" label="Location" placeholder="Location"></md-outlined-text-field>
       <md-outlined-text-field v-model="purpose" label="Description" type="textarea" placeholder="Description" rows="3" class="description-field"></md-outlined-text-field>
