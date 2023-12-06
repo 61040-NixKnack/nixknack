c526dcf79a85ee2eddaddf886b73a732929d5069
Caleb Frieson <cnf@mit.edu>
2023-12-05 21:21:52 -0500

    added template for Plan/Task

--- client/views/PlanView.vue
+++ client/views/PlanView.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import PostListComponent from "@/components/Post/PostListComponent.vue";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
+import PlanList from "@/components/Plan/PlanList.vue";
 
 const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
 </script>
 
 <template>
   <main>
-    <h1>Home Page</h1>
-    <section>
-      <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
-      <h1 v-else>Please login!</h1>
-    </section>
-    <PostListComponent />
+    <h1>Your Plan</h1>
+    <PlanList />
   </main>
 </template>
 
