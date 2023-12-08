4b6cf615efc53ec798b06e47a4733c5c6a0b7591
Rachel Lu <lur@MIT.EDU>
2023-12-08 03:19:17 -0500

    task format

--- client/components/Tasks/TaskList.vue
+++ client/components/Tasks/TaskList.vue
@@ -21,9 +21,7 @@ onBeforeMount(async () => {
       <h2>{{ dateName[(taskInd - 1 + offset) % 7] }}</h2>
       <div v-if="userPlan[taskInd - 1].length > 0">
         <div v-for="item in userPlan[taskInd - 1]" :key="item">
-          <p>• <b>Task:</b>
-            <p>Item: {{ item.item.name }}</p> 
-            <p>{{ item.objective }}</p></p>
+          <p>• <b>Task:</b>Item: {{ item.item.name }} {{ item.objective }}</p>
         </div>
       </div>
       <div v-else>
