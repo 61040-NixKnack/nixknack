4626eea034c23e7b8482d6d726ca864f3352796b
Adrian Anaemeje <anaemeje@mit.edu>
2023-12-13 14:36:56 -0500

    Add loading screen

--- client/components/Tasks/TaskList.vue
+++ client/components/Tasks/TaskList.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { onBeforeMount, ref } from "vue";
 import { fetchy } from "../../utils/fetchy";
+import "@material/web/progress/circular-progress.js";
 
 // Assuming items to get rid of today are stored in userPlan.value[0]
 const userPlan = ref([[], [], [], [], [], [], []]);
+const userPlanLoaded = ref(false);
 const dateName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const offset = new Date().getDay();
@@ -12,11 +14,12 @@ onBeforeMount(async () => {
   // Get user plan from server
   let a = await fetchy("/api/plans", "POST");
   userPlan.value = await fetchy("/api/plans", "GET");
+  userPlanLoaded.value = true;
 });
 </script>
 
 <template>
-  <main>
+  <main class="true-main" v-if="userPlanLoaded">
     <section v-for="taskInd in 7" :key="taskInd" class="secondary-div">
       <h2>{{ dateName[(taskInd - 1 + offset) % 7] }}</h2>
       <div v-if="userPlan[taskInd - 1].length > 0">
@@ -30,10 +33,13 @@ onBeforeMount(async () => {
       </div>
     </section>
   </main>
+  <main v-else>
+    <md-circular-progress indeterminate></md-circular-progress>
+  </main>
 </template>
 
 <style scoped>
-main {
+.true-main {
   display: flex;
   flex-direction: column;
   gap: 32px;
@@ -48,5 +54,11 @@ main {
   border: solid var(--secondary) 20px;
   border-top: 10px;
   border-bottom: 10px;
+}
+
+md-circular-progress {
+  position: fixed;
+  top: 50%;
+  left: 50%;
 }
 </style>
