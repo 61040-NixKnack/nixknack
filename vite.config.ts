959c6a675052a961f6b497ec33fcca24538aacb6
Adrian Anaemeje <anaemeje@mit.edu>
2023-12-13 15:12:32 -0500

    Add progress bar as custom element

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     vue({
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => ["md-circular-progress", "md-outlined-text-field", "md-filled-button", "md-select-option", "md-outlined-select", "md-filter-chip", "md-chip-set"].includes(tag),
+          isCustomElement: (tag) =>
+            ["md-circular-progress", "md-outlined-text-field", "md-filled-button", "md-select-option", "md-outlined-select", "md-filter-chip", "md-chip-set", "md-linear-progress"].includes(tag),
         },
       },
     }),
